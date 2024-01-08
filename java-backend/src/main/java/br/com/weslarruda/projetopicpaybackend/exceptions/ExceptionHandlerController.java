74e1d96701324b8b4e207158ac114d63efb5099d
weslArruda <weslleyarrudalemospinheiro@outlook.com>
2024-01-08 00:30:27 -0300

    docker and exceptions config

--- java-backend/src/main/java/br/com/weslarruda/projetopicpaybackend/exceptions/ExceptionHandlerController.java
+++ java-backend/src/main/java/br/com/weslarruda/projetopicpaybackend/exceptions/ExceptionHandlerController.java
@@ -1,5 +1,12 @@
 package br.com.weslarruda.projetopicpaybackend.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,11 +15,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class ExceptionHandlerController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        
+    private MessageSource messageSource;
 
+    public ExceptionHandlerController(MessageSource message) {
+        this.messageSource = message;
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+        
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+            ErrorMessageDTO error = new ErrorMessageDTO(err.getField(), message);
+            dto.add(error);
+        });
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
     
     
 }
--- java-backend/src/main/java/br/com/weslarruda/projetopicpaybackend/modules/user/UserEntity.java
+++ java-backend/src/main/java/br/com/weslarruda/projetopicpaybackend/modules/user/UserEntity.java
@@ -1,20 +1,29 @@
 package br.com.weslarruda.projetopicpaybackend.modules.user;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "users")
 public class UserEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^[^\\d\\s]+$", message = "O usuário não deve conter espaços")
+    @Pattern(regexp = "\\S+", message = "O usuário não deve conter espaços")
     private String username;
 
     @Length(min = 4, max = 80)
@@ -25,5 +34,8 @@ public class UserEntity {
 
     @Length(max = 11)
     private String cpf;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
